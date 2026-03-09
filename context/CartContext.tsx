a23c7f960d20473038afcf8ca87e44d926a042bd
Idem Allexandre <idementsinda1@gmail.com>
2026-03-09 23:57:15 +0100

    Implement CartContext for managing shopping cart

---
+++ context/CartContext.tsx
@@ -0,0 +1,39 @@
+"use client";
+import React, { createContext, useContext, useState, useEffect } from 'react';
+
+type CartItem = {
+  id: string;
+  name: string;
+  price: number;
+  quantity: number;
+};
+
+type CartContextType = {
+  cart: CartItem[];
+  addToCart: (item: CartItem) => void;
+  totalFCFA: number;
+};
+
+const CartContext = createContext<CartContextType | undefined>(undefined);
+
+export const CartProvider = ({ children }: { children: React.ReactNode }) => {
+  const [cart, setCart] = useState<CartItem[]>([]);
+
+  const addToCart = (item: CartItem) => {
+    setCart((prev) => [...prev, item]);
+  };
+
+  const totalFCFA = cart.reduce((acc, item) => acc + (item.price * item.quantity), 0);
+
+  return (
+    <CartContext.Provider value={{ cart, addToCart, totalFCFA }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export const useCart = () => {
+  const context = useContext(CartContext);
+  if (!context) throw new Error("useCart must be used within a CartProvider");
+  return context;
+};
